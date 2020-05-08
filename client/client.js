8a6e25adc39fecb0e1441018bc01e1047e7a42d1
Vladimir Tambovtsev <tambovcev99@mail.ru>
2020-05-09 00:29:20 +0300

    set bidirect stream

--- client/client.js
+++ client/client.js
@@ -58,8 +58,79 @@ function callGreetManyTimes() {
   })
 }
 
+function callLongGreeting() {
+  const address = 'localhost:50051'
+  const client = new service.GreetServiceClient(
+    address,
+    grpc.credentials.createInsecure()
+  )
+
+  const req = new greets.LongGreetRequest()
+  const call = client.longGreet(req, (err, res) => {
+    if (!err) {
+      console.log('Server response: ', res.getResult())
+    } else {
+      console.log(err)
+    }
+  })
+  let count = 0,
+    intervalID = setInterval(() => {
+      console.log('Sending message ', count)
+      const res = new greets.LongGreetRequest()
+      const greeting = new greets.Greeting()
+      greeting.setFirstName('Paulo')
+      greeting.setLastName('Brown')
+
+      req.setGreet(greeting)
+      call.write(req)
+
+      if (++count > 3) {
+        clearInterval(intervalID)
+        call.end()
+      }
+    }, 1000)
+}
+
+const sleep = async () =>
+  new Promise((resolve) => setTimeout(() => resolve(), 1000))
+
+async function callBiDirect() {
+  const address = 'localhost:50051'
+  const client = new service.GreetServiceClient(
+    address,
+    grpc.credentials.createInsecure()
+  )
+
+  const call = client.greetEveryone(_, (err, res) => {
+    console.log('Server response: ', res)
+  })
+
+  call.on('data', (res) => {
+    console.log('Hello bidirect: ', res.getResult())
+  })
+
+  call.on('error', (err) => console.log(err))
+  call.on('end', () => console.log('Bidirect Streams end'))
+
+  for (let i = 0; i < 10; i++) {
+    const greeting = new greets.Greeting()
+    greeting.setFirstName('Steve')
+    greeting.setLastName('Jobs')
+
+    const req = new greets.GreetEveryoneRequest()
+    req.setGreet(greeting)
+
+    call.write(req)
+
+    await sleep(1000)
+  }
+  call.end()
+}
+
 const main = () => {
   callGreetings()
   callGreetManyTimes()
+  callLongGreeting()
+  // callBiDirect()
 }
 main()
--- server/index.js
+++ server/index.js
@@ -29,10 +29,62 @@ function greetManyTimes(call, cb) {
     }, 1000)
 }
 
+// streaming cient API
+function longGreet(call, cb) {
+  call.on('data', (req) => {
+    const fullname =
+      req.getGreet().getFirstName() + ' ' + req.getGreet().getLastName()
+    console.log('Hello ', fullname)
+  })
+
+  call.on('error', (err) => {
+    console.log(err)
+  })
+
+  call.on('end', () => {
+    const res = new greets.LongGreetResponse()
+    res.setResult('Long Greet response end')
+    cb(null, res)
+  })
+}
+
+const sleep = async () =>
+  new Promise((resolve) => setTimeout(() => resolve(), 1000))
+
+// streaming bi-directional
+async function greetEveryone(call, cb) {
+  call.on('data', (res) => {
+    const fullname =
+      res.getGreet().getFirstName() + ' ' + res.getGreet().getLastName()
+    console.log('bidirectional fullname is ', fullname)
+  })
+
+  call.on('error', (err) => console.log(err))
+  call.on('end', () => console.log('Bi-directional streaming ends..'))
+
+  for (let i = 0; i < 10; i++) {
+    const greeting = new greets.Greeting()
+    greeting.setFirstName('Drake')
+    greeting.setLastName('Cool')
+
+    const req = new greets.GreetEveryoneRequest()
+    req.setGreet(greeting)
+
+    call.write(req)
+    await sleep(1000)
+  }
+  call.end()
+}
+
 const main = (params) => {
   const server = new grpc.Server()
 
-  server.addService(service.GreetServiceService, { greet, greetManyTimes })
+  server.addService(service.GreetServiceService, {
+    greet,
+    greetManyTimes,
+    longGreet,
+    // greetEveryone,
+  })
 
   const address = '127.0.0.1:50051'
   server.bind(address, grpc.ServerCredentials.createInsecure())
